f6022ad66551c71ef32057c168ef5ad84a9a4766
Tony <397013181@qq.com>
2015-07-29 17:49:36 +0800

    网页

--- js/web.js
+++ js/web.js
@@ -3,35 +3,62 @@ var server_address = "www.kingdonsoft.com:8003";
 var virtualdir_user = "KDOAWeb";
 var interface_user = "webservice/mobile.asmx";
 
-//var function get
-
-
-function getWorklogPaged(count){
-		var data = '<?xml version="1.0" encoding="UTF-8"?>';
-	    data = data + '<soap:Envelope xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:soap="http://schemas.xmlsoap.org/soap/envelope/">';
-	    data = data + '<soap:Body>';
-	    
-	    data = data + '<Login xmlns="http://tempuri.org/">';
-	    
-	    data = data + '<GetWorkTypeList xmlns="http://tempuri.org/">';
-	    data = data + '<updateTime>string</updateTime>';
-	    data = data + '<userId>int</userId>';
-	    data = data + '<token>string</token>';
-	    data = data + '</GetWorkTypeList>';
-	    data = data + '</soap:Body>';
-	    data = data + '</soap:Envelope>';   
-	
-		var xhr;
-		if(window.ActiveXObject){
-			xhr = new ActiveXObject("Microsoft.XMLHTTP");
-		}else if(window.XMLHttpRequest){
-			xhr = new XMLHttpRequest()
-		}
-		xhr.open("POST","http://kingdonsoft.com:88/KDOAWeb/webservice/mobile.asmx/GetWorkTypeList", false);
-		xhr.setRequestHeader("CONTENT-TYPE","text/xml; charset=utf-8");
-	    xhr.setRequestHeader ("SOAPAction","http://tempuri.org/Login");
-	    xhr.send(data);
-	    
-	    var xmlDoc = xmlhttp.responseXML;
-	
+function getActionUrl(methord){
+	return "http://"
+		+ server_address
+		+ "/"
+		+ virtualdir_user 
+		+ "/" 
+		+ interface_user 
+		+ "/" 
+		+ methord;
 }
+
+
+	function getWorklogPaged(count){
+//		var soapMessage =
+//	　　'<soap:Envelope xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:soap="http://schemas.xmlsoap.org/soap/envelope/"> \
+//		　　<soap:Body> \
+//		　　<Login xmlns="http://tempuri.org/"> \
+//			　　<UserName>lhou</UserName> \
+//			　　<Pswd>ou852723</Pswd> \
+//		　　</Login> \
+//		　　</soap:Body> \
+//	　　</soap:Envelope>';
+	
+//		$.ajax({
+//			type:"post",
+//			url:getActionUrl("Login"),
+////	　　　　 contentType: "Application/Json",
+//	　　　　 contentType: "jsonp",
+//	　　　　	data:{UserName:"lhou",Pswd:"ou852723"},
+//	
+//			success: function(data) {
+//				var jsonValue = data;
+//              alert(jsonValue.d);// 输出Json
+//			},
+//			error: function(xhr, type, errorThrown) {
+//				//异常处理；
+////				alert("error");
+//			},
+//			complete: function(XMLHttpRequest, textStatus) {
+//              var returnText = XMLHttpRequest.responseText;
+//              alert("complete \n" + returnText);
+//          }
+//		});
+
+
+$.ajax({
+type: 'GET',
+url: 'http://www.pm25.in/api/querys/aqi_details.json?city=wenzhou&token=', 
+dataType: 'jsonp',
+success: function(msg){
+alert(JSON.stringify(msg));
+},
+error:function(){
+alert('error');
+}
+});
+		
+	}
+
