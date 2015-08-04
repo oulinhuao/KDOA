a2be8c34262ef60d6c395f9dc90b03b2d63729c5
Tony <397013181@qq.com>
2015-08-04 17:28:37 +0800

    jQuery选择器学习 checkbox样式美化 js方法练习，由一个checkbox的控制另一个 checkbox span 数值方向对齐（暂用margin）

--- js/index.js
+++ js/index.js
@@ -1,8 +1,3 @@
-/*
-	Custom checkbox and radio button - Jun 18, 2013
-	(c) 2013 @ElmahdiMahmoud 
-	license: http://www.opensource.org/licenses/mit-license.php
-*/   
 $('input[name="radio-btn"]').wrap('<div class="radio-btn"><i></i></div>');
 $(".radio-btn").on('click', function () {
     var _this = $(this),
@@ -13,11 +8,58 @@ $(".radio-btn").on('click', function () {
     _this.find('input:radio').attr('checked', true);
 });
 
-$('input[name="check-box"]').wrap('<div class="check-box"><i></i></div>');
+$('input[name="myckb_autoKeepPwd"]').wrap('<div class="myckb_autoKeepPwd"><i></i></div>');
+$('input[name="myckb_autoLogin"]').wrap('<div class="myckb_autoLogin"><i></i></div>');
 $.fn.toggleCheckbox = function () {
     this.attr('checked', !this.attr('checked'));
+    return this.attr('checked');
 }
-$('.check-box').on('click', function () {
+
+
+$('.myckb_autoLogin').on('click',function () {
+    var isChecked = $(this).find(':checkbox').toggleCheckbox();
+    $(this).toggleClass('checkedBox');
+    
+   	if(isChecked == 'checked'){
+   		var keepCkb = $('.myckb_autoKeepPwd');
+	   	if(keepCkb.find(':checkbox').attr('checked') != 'checked'){
+	   		keepCkb.find(':checkbox').toggleCheckbox();
+	    	keepCkb.toggleClass('checkedBox');
+	   	}
+   	}
+});
+
+$('.myckb_autoKeepPwd').on('click',function () {
     $(this).find(':checkbox').toggleCheckbox();
     $(this).toggleClass('checkedBox');
+    
 });
+
+
+
+
+/**
+ * 自动登录选择事件
+ * @param {Object} isChecked
+ */
+function autoLogin(isChecked,chkObj){
+	chkObj.attr('checked', isChecked);
+	if(isChecked){
+		alert("选中了");
+	}else{
+		alert("没");
+	}
+}
+
+/**
+ * 
+ * @param {Object} isKeep
+ */
+function keepPwd(isKeep){
+	if(checked){
+		alert("记住密码");
+	}else{
+		alert("不记住");
+	}
+}
+
